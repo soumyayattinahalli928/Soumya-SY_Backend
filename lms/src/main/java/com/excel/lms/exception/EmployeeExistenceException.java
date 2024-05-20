7eda13875ab88b36a93cd85b7fc0244a31440e04
soumyayattinahalli928 <soumyayattinahalli928@gmail.com>
2024-05-20 19:00:27 +0530

    lms

--- lms/src/main/java/com/excel/lms/exception/EmployeeExistenceException.java
+++
@@ -1,8 +0,0 @@
-package com.excel.lms.exception;
-
-public class EmployeeExistenceException extends RuntimeException {
-
-	public EmployeeExistenceException(String message) {
-		super(message);
-	}
-}
